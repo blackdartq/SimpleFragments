9f6f9ca02da9a7f02068af14cd57322db0763f18
Stetson Pierce <stetsmando@gmail.com>
2015-11-27 22:18:32 -0700

    Inital Commit

---
+++ app/src/main/java/com/stetson_pierce/simplefragments/MainActivity.java
@@ -0,0 +1,26 @@
+package com.stetson_pierce.simplefragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void doStuff(View view){
+        TextView textView = (TextView ) findViewById(R.id.textView);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        textView.setText(editText.getText());
+        
+    }
+
+
+
+
+}
